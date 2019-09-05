71ffc5b3ba7f377d66e3416c267414907d96c86f
Kerry Jackson <kerry.jackson108@gmail.com>
2019-09-05 12:46:56 -0400

    updates to forms and modal component

--- src/components/AdoptionApplication.js
+++ src/components/AdoptionApplication.js
@@ -6,7 +6,7 @@ class AdoptionApplication extends Component {
     return (
       <section>
         <div class="form-style-5">
-          <form>
+          <form method="POST" action={global.gConfig.baseAPIURL + "/adopt"}>
             <h1>Adoption Application</h1>
             <p>
               After submitting the form below, please allow 48 hours for one of
@@ -15,20 +15,27 @@ class AdoptionApplication extends Component {
             </p>
             <fieldset>
               <legend>
-                <span class="number">1</span> Personal Info
+                <span className="number">1</span> Personal Info
               </legend>
+              <input type="text" name="name" placeholder="Full Name" required />
+              <input type="email" name="email" placeholder="Email" required />
               <input
-                type="text"
-                name="field1"
-                placeholder="Full Name"
+                type="tel"
+                name="phone"
+                placeholder="Phone"
+                // pattern="[0-9]{3} [0-9]{3} [0-9]{4}"
+                maxlength="12"
                 required
               />
-              <input type="email" name="field2" placeholder="Email" required />
-              <input type="text" name="field3" placeholder="Phone" required />
-              <input type="text" name="field4" placeholder="Address" required />
-              <input type="text" name="field4" placeholder="Address 2" />
-              <input type="text" name="field4" placeholder="City" required />
-              <select>
+              <input
+                type="text"
+                name="address"
+                placeholder="Address"
+                required
+              />
+              <input type="text" name="address2" placeholder="Address 2" />
+              <input type="text" name="city" placeholder="City" required />
+              <select name="state" required>
                 <option value="AL">Alabama</option>
                 <option value="AK">Alaska</option>
                 <option value="AZ">Arizona</option>
@@ -83,33 +90,34 @@ class AdoptionApplication extends Component {
               </select>
               <input
                 type="text"
-                name="field4"
+                name="occupation"
                 placeholder="Occupation"
                 required
               />
               <textarea
-                name="field3"
+                name="why_adopt"
                 placeholder="Why do you want to bring a new dog/puppy into your home?"
                 required
               />
               <input
                 type="text"
-                name="field4"
+                name="dog_name"
                 placeholder="Name of dog you are interested in?"
+                maxLength="20"
                 required
               />
             </fieldset>
             <fieldset>
               <legend>
-                <span class="number">2</span> References
+                <span className="number">2</span> References
               </legend>
               <p>
                 Please provide two personal references. Include their name,
                 phone number and how this person knows you and your ability to
                 provide a good home for a dog
               </p>
-              <textarea name="field3" placeholder="Reference #1" required />
-              <textarea name="field3" placeholder="Reference #2" required />
+              <textarea name="reference1" placeholder="Reference #1" required />
+              <textarea name="reference2" placeholder="Reference #2" required />
             </fieldset>
             <p>
               Please provide the contact information of a veterinarian you have
@@ -119,12 +127,24 @@ class AdoptionApplication extends Component {
               <span>Vet references are checked.</span>
             </p>
 
-            <input type="text" name="field4" placeholder="Vet Name" required />
-            <input type="text" name="field4" placeholder="Vet Phone" required />
+            <input
+              type="text"
+              name="vet_name"
+              placeholder="Vet Name"
+              required
+            />
+            <input
+              type="tel"
+              name="vet_phone"
+              placeholder="Vet Phone"
+              // pattern="[0-9]{3} [0-9]{3} [0-9]{4}"
+              maxlength="12"
+              required
+            />
 
             <fieldset>
               <legend>
-                <span class="number">3</span> Household Info
+                <span className="number">3</span> Household Info
               </legend>
               <p>
                 Please list place of employment for yourself and your
@@ -133,23 +153,28 @@ class AdoptionApplication extends Component {
               </p>
               <input
                 type="text"
-                name="field4"
+                name="employer"
                 placeholder="My Employer & Location"
                 required
               />
               <input
                 type="text"
-                name="field4"
+                name="spouse_employer"
                 placeholder="Spouse's Employer & Location"
               />
 
-              <div className="radio-container">
+              <div className="radio-container" name="residence_type" required>
                 Residence:
-                <input name="own" type="radio" value="own" checked="checked" />
+                <input
+                  name="residence_own"
+                  type="radio"
+                  value="own"
+                  checked="checked"
+                />
                 Own
-                <input name="rent" type="radio" value="rent" />
+                <input name="residence-rent" type="radio" value="rent" />
                 Rent
-                <input name="parents" type="radio" value="parents" />
+                <input name="residence-parents" type="radio" value="parents" />
                 Live with Parents
               </div>
               <p>
@@ -167,14 +192,21 @@ class AdoptionApplication extends Component {
               </p>
               <input
                 type="text"
-                name="field4"
-                placeholder="Landlord Name & Contact"
+                name="landlord_name"
+                placeholder="Landlord Name"
+              />
+              <input
+                type="tel"
+                name="landlord_contact"
+                placeholder="Landlord Contact"
+                MaxLength="12"
+                // pattern="[0-9]{3} [0-9]{3} [0-9]{4}"
               />
 
               <p>
                 Total number of people living in your home, including yourself:
               </p>
-              <select>
+              <select name="total_people_residence" required>
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -182,19 +214,24 @@ class AdoptionApplication extends Component {
                 <option value="5+">5+</option>
               </select>
 
-              <div className="radio-container">
+              <div className="radio-container" name="children" required>
                 Do you currently have children living at home?
-                <input name="yes" type="radio" value="yes" checked="checked" />
+                <input
+                  name="children"
+                  type="radio"
+                  value="yes"
+                  checked="checked"
+                />
                 Yes
-                <input name="no" type="radio" value="no" />
+                <input name="children" type="radio" value="no" />
                 No
               </div>
 
-              <div className="radio-container">
+              <div className="radio-container" name="cats">
                 Do you have one or more cats?
-                <input name="yes" type="radio" value="yes" checked="checked" />
+                <input name="cats" type="radio" value="yes" checked="checked" />
                 Yes
-                <input name="no" type="radio" value="no" />
+                <input name="cats" type="radio" value="no" />
                 No
               </div>
             </fieldset>
