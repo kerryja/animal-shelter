84b39f9abfe4ee73a28655751251ff70ca500950
Kerry Jackson <kerry.jackson108@gmail.com>
2019-09-20 15:10:41 -0400

    update to homepage -responsive picture and text

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,12 @@ class Home extends Component {
   render() {
     return (
       <section className="hero">
-        <h1 className="text-white hero-text text-left">
+        <h1 className="text-white hero-text text-xs-center text-md-left">
           <p>Adopt a dog,</p>
           <p>Save a life</p>
+        </h1>
+        <h1 className="text-white text-xs-center hero-text-mobile">
+          <p>Adopt a dog, save a life.</p>
         </h1>
       </section>
     );
